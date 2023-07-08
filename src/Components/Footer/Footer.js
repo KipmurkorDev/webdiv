91b708502a499fd9d0b048f18f41a91c28317c65
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-07-08 15:08:22 +0300

    .

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub, faPhone} from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faPhone } from '@fortawesome/free-brands-svg-icons'
 
 import './footer.css'
 const Footer = () => {
@@ -11,64 +11,51 @@ const Footer = () => {
     // <FontAwesomeIcon icon={faPhone} />
     return (
         <div class="footer-distributed">
+              <hr/>
+              <>
             <div class="footer-left">
-
-                <h3>WebDiv Technology<span></span></h3>
-                <p class="footer-links">
-                    <a href="/#" class="link-1">Home</a>
-
+                <div class="footer-company-about"><strong> WebDiv Technology</strong></div>
+                <div class="footer-links">
+                    <a href="/#">Home</a>
                     <a href="/#">Services</a>
-
                     <a href="/#">Case Studies</a>
-
                     <a href="/#">Contact Us</a>
-                </p>
-
-                <p class="footer-company-name">WebDiv Techology Ltd ©2023</p>
+                </div>
             </div>
             <div class="footer-center">
-            <div class="footer-company-about"><strong> Headquarters</strong></div>
-
-                <div>
+                <div class="footer-company-about"><strong> Headquarters</strong></div>
+                <div style={{}}>
                     <p><span>29, Kabiyet</span> Rift Valley, Kenya</p>
                 </div>
-
                 <div>
-                <FontAwesomeIcon icon="fa-solid fa-phone" />                 
-                <p>+254717330289</p>
+                    <FontAwesomeIcon icon="fa-solid fa-phone" />
+                    <p>+254717330289</p>
                 </div>
 
                 <div>
-                    {}
                     <p><a href="mailto:webdivtechology@gmail.com">webdivtechology@gmail.com</a></p>
                 </div>
 
             </div>
 
             <div class="footer-right">
-
                 <p class="footer-company-about"><strong> Company</strong></p>
                 <div className='link'>
                     <a href='/'>About Us</a>
                     <a href='/'>Privacy</a>
                     <a href='/'>Terms and Conditions</a>
                     <a href='/'>Collections and refund policy</a>
-
-
                 </div>
-
-                <div class="footer-icons">
-
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/KipmurkorDev" className="link">
-                        {element2}
-                    </a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kipmurkor-emmanuel/" className="link">
-                        {element}
-                    </a>
-
-
+                
+            </div> 
+            <hr/>
+            </>
+            <div className='rights-1'> 
+            <div><p class="footer-company-name">WebDiv Techology Ltd ©2023</p> </div>
+            <div class="footer-icons">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/KipmurkorDev" className="link"> {element2} </a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kipmurkor-emmanuel/" className="link"> {element}</a>
                 </div>
-
             </div>
 
         </div>
--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -3,13 +3,10 @@ import './home.css'
 const HomePage = () => {
   return (
     <div className='home'>
-    
-Build or scale up your
-software development
-team
-We help you simplify your software development lifecycle by setting up super productive agile development teams
 
-Know More
+      Build or scale up your
+      software development team
+      We help you simplify your software development lifecycle by setting up super productive agile development teams Know More\
     </div>
   )
 }
