7a00d1e4603ec24e14c4e885ab657df473d50f7d
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-08-02 19:49:23 +0300

    modal

--- src/Components/Model/Model.js
+++ src/Components/Model/Model.js
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './model.css'
 function Model() {
+    const [open, setOpen]=useState(false)
     return (
         <div>
-        <button className='link-nav contact'>Contact Us</button>
-
+        <button className='link-nav contact' onClick={()=>setOpen(!open)}>Contact Us</button>
+        <div className={`${open === false ? 'modal' : ''}`}>
+            <form className='modal-content'>            
+            <lab> Name</lab>
+            <input type='text' />
+            </form>
+        </div>
         </div>
     )
 }
